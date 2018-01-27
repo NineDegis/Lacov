edcb58cd05b5bf0eceeab922103bc8c45b49e675
WooYongHa <wyh951753@gmail.com>
2018-01-27 22:20:39 +0900

    implment extract module and add readme

--- extract_sound/extract_sound.py
+++ extract_sound/extract_sound.py
@@ -1,24 +1,6 @@
 from __future__ import unicode_literals
 import youtube_dl
 
-"""
-#format
-    best: best quality format represented by a single file with video and audio.
-    worst: worst quality format represented by a single file with video and audio.
-    bestvideo: best quality video-only format (e.g. DASH video). 
-    worstvideo: worst quality video-only format. 
-    bestaudio: best quality audio only-format.
-    worstaudio: worst quality audio only-format.
-
-#codec
-   "best"(default) --> original codec
-   "aac", "flac", "mp3", "m4a", "opus", "vorbis", "wav"
-    mp3 requires additional 'libmp3lame' library
-   
-#quality
-    0(best) ~ 9(worst) or bitrate
-"""
-
 ydl_opts = {
     'format': 'bestaudio/best',
     'postprocessors': [{
